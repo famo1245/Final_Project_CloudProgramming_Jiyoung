c76da10fde91562b00c390d12e49145cb02d85be
famo1245 <kandallee38@gmail.com>
2023-06-08 01:11:46 +0900

    한글 url 추가

--- search/views.py
+++ search/views.py
@@ -1,22 +1,24 @@
 from django.shortcuts import render
+from django.conf import settings
 import urllib.request
 import urllib.parse
 import json
 
 
 def index(request):
-    return render(request, 'search/search-main.html',)
+    return render(request, 'search/search-main.html', )
 
 
 def result(request):
     player_exist = False
     player_info = {}
-    api_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJYLUFwcC1SYXRlLUxpbWl0IjoiNTAwOjEwIiwiYWNjb3VudF9pZCI6IjEzMjU2MTE2MzQiLCJhdXRoX2lkIjoiMiIsImV4cCI6MTcwMTU5Mjk5NCwiaWF0IjoxNjg2MDQwOTk0LCJuYmYiOjE2ODYwNDA5OTQsInNlcnZpY2VfaWQiOiI0MzAwMTE0ODEiLCJ0b2tlbl90eXBlIjoiQWNjZXNzVG9rZW4ifQ.3HWJSkPO9qngikbocORmWwI_fMlPoMMf3pqECltXqkI"
+    api_key = settings.NEXON_KEY
     if request.method == "GET":
         player_name = request.GET.get('search_text')
         print(player_name)
-        # encText = urllib.parse.quote("utf-8", format(player_name))
-        request_url = "https://api.nexon.co.kr/fifaonline4/v1.0/users?nickname=" + player_name
+        # 한국어 닉네임의 경우 ascii로 변환해줘야 함
+        enc_name = urllib.parse.quote(player_name)
+        request_url = "https://api.nexon.co.kr/fifaonline4/v1.0/users?nickname=" + enc_name
         api_request = urllib.request.Request(request_url)
         api_request.add_header("Authorization", api_key)
         response = urllib.request.urlopen(api_request)
@@ -33,14 +35,14 @@ def result(request):
                 player_info['level'] = player_result['level']
 
                 return render(request, 'search/search-result.html',
-                        {
-                            'player_exist': player_exist,
-                            'player_info': player_info,
-                            'input_name': player_name
-                        })
+                              {
+                                  'player_exist': player_exist,
+                                  'player_info': player_info,
+                                  'player_name': player_name
+                              })
 
     if not player_exist:
         return render(request, 'search/search-result.html',
-                {
-                    'player_exist': player_exist,
-                })
+                      {
+                          'player_exist': player_exist,
+                      })
