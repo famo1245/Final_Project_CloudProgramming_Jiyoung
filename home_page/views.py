74a4362b6293c952cedc611daa1980475968222a
famo1245 <kandallee38@gmail.com>
2023-06-17 21:30:33 +0900

    홈페이지에서 최신 게시글 볼수있게 수정

--- home_page/views.py
+++ home_page/views.py
@@ -3,8 +3,13 @@ from board.models import Post, Category
 
 
 def landing(request):
+    duo_list = Post.objects.filter(category__slug='듀오').order_by('-pk')[:5]
+    squad_list = Post.objects.filter(category__slug='스쿼드').order_by('-pk')[:5]
+    free_list = Post.objects.filter(category__slug='자유').order_by('-pk')[:5]
     return render(request,
                   'home_page/index.html',
                   {
-                      'categories': Category.objects.all(),
+                      'duo_list': duo_list,
+                      'squad_list': squad_list,
+                      'free_list': free_list,
                   })
